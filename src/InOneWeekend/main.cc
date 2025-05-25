880a2aa498486da98d47abfee128edadfa17e874
sh1027 <shiori.ueda@keio.jp>
2025-05-25 15:23:40 +0200

    Reformat file

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int main() {
-
+int main()
+{
     // Image
 
     int image_width = 256;
@@ -9,12 +9,15 @@ int main() {
 
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) {
-        for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
+    for (int j = 0; j < image_height; j++)
+    {
+        for (int i = 0; i < image_width; i++)
+        {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
             auto b = 0.0;
 
             int ir = int(255.999 * r);
