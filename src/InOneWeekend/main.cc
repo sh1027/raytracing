cd1a042752512eaae9eaf87c631f620b92664946
sh1027 <shiori.ueda@keio.jp>
2025-05-25 15:25:37 +0200

    Done 2.3. Adding a Progress Indicator

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -14,6 +14,7 @@ int main()
 
     for (int j = 0; j < image_height; j++)
     {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++)
         {
             auto r = double(i) / (image_width - 1);
@@ -27,4 +28,6 @@ int main()
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::clog << "\rDone.                 \n";
 }
